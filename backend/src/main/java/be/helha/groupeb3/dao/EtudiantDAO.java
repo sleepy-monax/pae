e2d8ed46dd409ae7c4074e44717de6dfa892daee
AiIurus <mathieu.trempont@hotmail.com>
2021-01-11 14:43:00 +0100

    Backend: Push dao for EJB

--- backend/src/main/java/be/helha/groupeb3/dao/EtudiantDAO.java
+++ backend/src/main/java/be/helha/groupeb3/dao/EtudiantDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import be.helha.groupeb3.entities.Etudiant;
 
@@ -14,14 +15,64 @@ public class EtudiantDAO {
 	@PersistenceContext(unitName = "groupeB3JTA")
 	private EntityManager em;
 	
+	private void commit() {
+    	em.getTransaction().begin();
+    	em.getTransaction().commit();
+    }
+	
 	public List<Etudiant> findAll() {
 		return em.createQuery("SELECT etudiant FROM Etudiant etudiant").getResultList();
 	}
 	
+	public Etudiant findByMatricule(String matricule) {
+		if (matricule == null) return null;
+		String requete = "SELECT etudiant FROM Etudiant etudiant "
+				+ "WHERE etudiant.matricule = ?1";
+				
+		TypedQuery<Etudiant> query = em.createQuery(requete, Etudiant.class);
+        query.setParameter(1, matricule);
+        List<Etudiant> result = query.getResultList();
+        return result.isEmpty() ? null : result.get(0);
+	}
+	
 	public Etudiant add(Etudiant etudiant) {
+		if (etudiant == null) return null;
+		if (findByMatricule(etudiant.getMatricule()) == null) return null;
+		
 		em.persist(etudiant);
+		commit();
 		
 		return null;
+	}	
+	
+	public void update(Etudiant eOld, Etudiant eNew) {
+		if (eOld == null || eNew == null || eOld.getId()==null) {
+        	return;
+        }
+        em.clear();
+        Etudiant e = findByMatricule(eOld.getMatricule());
+        if (e == null) {   
+        	return;
+        }
+        
+        e = eNew;
+        e.setId(eOld.getId());  
+        
+        em.merge(e);
+        
+        commit();
+	}
+	
+	public void remove(Etudiant etudiant) {
+		if (findByMatricule(etudiant.getMatricule()) == null) return;
+		String requete = "DELETE etudiant FROM Etudiant etudiant "
+				+ "WHERE etudiant.matricule = ?1";
+				
+		TypedQuery<Etudiant> query = em.createQuery(requete, Etudiant.class);
+        query.setParameter(1, etudiant.getMatricule());
+        
+        commit();
+        return;
 	}
 
 }
--- backend/src/main/java/be/helha/groupeb3/dao/UtilisateurDAO.java
+++ backend/src/main/java/be/helha/groupeb3/dao/UtilisateurDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import be.helha.groupeb3.entities.Utilisateur;
 
@@ -14,14 +15,55 @@ public class UtilisateurDAO {
 	@PersistenceContext(unitName = "groupeB3JTA")
 	private EntityManager em;
 	
+	private void commit() {
+    	em.getTransaction().begin();
+    	em.getTransaction().commit();
+    }
+	
 	public List<Utilisateur> findAll() {
 		return em.createQuery("SELECT utilisateur FROM Utilisateur utilisateur").getResultList();
 	}
 	
+	public Utilisateur findById(int id) {
+		return em.find(Utilisateur.class, id);
+	}
+	
 	public Utilisateur add(Utilisateur utilisateur) {
+		if (utilisateur == null) return null;
+		if (findById(utilisateur.getId()) == null) return null;
 		em.persist(utilisateur);
-		
-		return null;
+		commit();
+        return null;
+	}
+	
+	public void update(Utilisateur uOld, Utilisateur uNew) {
+		if (uOld == null || uNew == null || uOld.getId()==null) {
+        	return;
+        }
+        em.clear();
+        Utilisateur u = findById(uOld.getId());
+        if (u == null) {   
+        	return;
+        }
+        
+        u = uNew;
+        u.setId(uOld.getId());  
+        
+        em.merge(u);
+        
+        commit();
+	}
+	
+	public void remove(Utilisateur utilisateur) {
+		if (findById(utilisateur.getId()) == null) return;
+		String requete = "DELETE utilisateur FROM Utilisateur utilisateur "
+				+ "WHERE utilisateur.id = ?1";
+				
+		TypedQuery<Utilisateur> query = em.createQuery(requete, Utilisateur.class);
+        query.setParameter(1, utilisateur.getId());
+        
+        commit();
+        return;
 	}
 
 }
