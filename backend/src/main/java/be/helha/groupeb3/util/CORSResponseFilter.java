6347c602b8d6d4f7bf563934d629281e6b41b6ae
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-14 13:37:58 +0100

    meta: C'est la faute de mathieu.

--- backend/src/main/java/be/helha/groupeb3/util/CORSResponseFilter.java
+++ backend/src/main/java/be/helha/groupeb3/util/CORSResponseFilter.java
@@ -14,17 +14,13 @@ public class CORSResponseFilter implements ContainerResponseFilter {
                        ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Origin", "*");
-        System.err.println("Voiture1");
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Credentials", "true");
-        System.err.println("Voiture2");
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Headers",
                 "origin, content-type, accept, authorization");
-        System.err.println("Voiture3");
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Methods",
                 "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        System.err.println("Voiture4");
     }
 }
