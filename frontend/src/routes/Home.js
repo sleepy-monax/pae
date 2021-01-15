b10422d3db15bd1a7c22d029cb1f17c31b30b741
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2021-01-15 15:12:07 +0100

    frontend: Fix home.

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -8,71 +8,73 @@ import { OutlineBlue } from "../components/Styles";
 import { FindAllSections } from "../services/SectionService";
 
 function Bloc(props) {
-  let bloc = props.bloc;
+    let bloc = props.bloc;
 
-  return (
-    <LinkButton
-      variante={OutlineBlue}
-      text={bloc.name}
-      to={"/bloc/" + bloc.id}
-    />
-  );
+    return (
+        <LinkButton
+            variante={OutlineBlue}
+            text={bloc.name}
+            to={"/bloc/" + bloc.id}
+        />
+    );
 }
 
 function Section(props) {
-  let section = props.section;
+    let section = props.section;
 
-  return (
-    <>
-      <div className="mt-4">{section.name}</div>
-      {section.blocs.map((bloc, index) => (
-        <Bloc key={index} bloc={bloc} />
-      ))}
-    </>
-  );
+    return (
+        <>
+            <div className="mt-4">{section.name}</div>
+            {section.blocs.map((bloc, index) => (
+                <Bloc key={index} bloc={bloc} />
+            ))}
+        </>
+    );
 }
 
 export default function Home() {
-  let [sections, setSections] = useState(false);
+    let [sections, setSections] = useState(false);
 
-  useEffect(() => {
-    if (!sections) {
-      FindAllSections().then((sections) => setSections(sections));
-    }
-  });
+    useEffect(() => {
+        if (!sections) {
+            FindAllSections()
+                .then((sections) => setSections(sections))
+                .catch(() => setSections(null));
+        }
+    });
 
-  let sectionsComponents = <Loading />;
+    let sectionsComponents = <Loading />;
 
-  if (sections === null) {
-    sectionsComponents = (
-      <Information
-        className="mt-4"
-        text="Aucunes données
+    if (sections === null) {
+        sectionsComponents = (
+            <Information
+                className="mt-4"
+                text="Aucunes données
         disponibles, veuillez en importer"
-      />
+            />
+        );
+    } else if (sections) {
+        sectionsComponents = sections.map((section, index) => (
+            <Section key={index} section={section} />
+        ));
+    }
+
+    return (
+        <div className="">
+            <div className="bg-white">
+                <img
+                    className="max-w-xs mx-auto py-8"
+                    src="/assets/curriculum.svg"
+                    alt=""
+                />
+            </div>
+            <div className="max-w-xl mx-auto flex flex-col gap-2 pb-8 px-4">
+                {sectionsComponents}
+
+                <div className="mt-4">Administration</div>
+
+                <LinkButton to="/import" text="Importer des données" />
+            </div>
+        </div>
     );
-  } else if (sections) {
-    sectionsComponents = sections.map((section, index) => (
-      <Section key={index} section={section} />
-    ));
-  }
-
-  return (
-    <div className="">
-      <div className="bg-white">
-        <img
-          className="max-w-xs mx-auto py-8"
-          src="/assets/curriculum.svg"
-          alt=""
-        />
-      </div>
-      <div className="max-w-xl mx-auto flex flex-col gap-2 pb-8 px-4">
-        {sectionsComponents}
-
-        <div className="mt-4">Administration</div>
-
-        <LinkButton to="/import" text="Importer des données" />
-      </div>
-    </div>
-  );
 }
