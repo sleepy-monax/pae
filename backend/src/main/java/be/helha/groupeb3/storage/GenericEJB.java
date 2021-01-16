0e31196481a97c03e547c97214e63458edf62af9
AiIurus <mathieu.trempont@hotmail.com>
2021-01-16 17:30:19 +0100

    backend: add commentary to EJBGeneric

--- backend/src/main/java/be/helha/groupeb3/storage/GenericEJB.java
+++ backend/src/main/java/be/helha/groupeb3/storage/GenericEJB.java
@@ -11,18 +11,22 @@ public class GenericEJB {
     @EJB
     private GenericDAO dao;
 
+    //Find all entities
     public <TEntity> List<TEntity> findAll(Class<TEntity> klass) {
         return dao.findAll(klass);
     }
 
+    //Return an entity by the id
     public <TEntity> TEntity findById(Class<TEntity> klass, Object id) {
         return dao.findById(klass, id);
     }
 
+    //Return entities compared to the field passed in parameter
     public <TEntity> List<TEntity> findAllByField(Class<TEntity> klass, String field, Object value) {
         return dao.findAllByField(klass, field, value);
     }
 
+    //Return the first entity found by calling the findAllByField function
     public <TEntity> TEntity findFirstByField(Class<TEntity> klass, String field, Object value) {
         var result = findAllByField(klass, field, value);
 
@@ -33,22 +37,27 @@ public class GenericEJB {
         }
     }
 
+    //Add an entity
     public <TEntity extends IEntity> TEntity add(Class<TEntity> klass, TEntity entity) {
         return dao.add(klass, entity);
     }
 
+    //Add an entity without checking the id
     public <TEntity extends IEntity> TEntity addFastAndUnsafe(Class<TEntity> klass, TEntity entity) {
         return dao.addFastAndUnsafe(klass, entity);
     }
 
+    //Update an entity with the new entity passed in parameter
     public <TEntity extends IEntity> boolean update(Class<TEntity> klass, TEntity oldEntity, TEntity newEntity) {
         return dao.update(klass, oldEntity, newEntity);
     }
 
+    //Update an entity
     public <TEntity extends IEntity> boolean update(Class<TEntity> klass, TEntity entity) {
         return dao.update(klass, entity);
     }
 
+    //Remove an entity
     public <TEntity extends IEntity> boolean remove(Class<TEntity> klass, TEntity entity) {
         return dao.remove(klass, entity);
     }
